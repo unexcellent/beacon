3f5908b80b99aa3e6c9c2c9292bd1c81372b4cd6
unexcellent <>
2026-05-26 13:57:06 +0200

    working audio output

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
 fn main() {
     embuild::espidf::sysenv::output();
+
+    let img = image::open("examples/patch.png").expect("open examples/patch.png");
+    let img = img.resize_exact(320, 240, image::imageops::FilterType::Lanczos3);
+    let rgb_bytes = img.to_rgb8().into_raw();
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_path = std::path::PathBuf::from(out_dir).join("patch_rgb.bin");
+    std::fs::write(&out_path, &rgb_bytes).expect("write patch_rgb.bin");
+
+    println!("cargo:rerun-if-changed=examples/patch.png");
 }
